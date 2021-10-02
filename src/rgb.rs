62787f8cb1cea6aa06b3b6fd5d74829ab98e9aa9
sgbasaraner <sarpbasaraner@gmail.com>
2021-10-02 22:13:07 +0300

    add rgb tests

--- src/rgb.rs
+++ src/rgb.rs
@@ -17,6 +17,34 @@ impl From<u32> for RGB {
     }
 }
 
+impl From<RGB> for u32 {
+    fn from(rgb: RGB) -> Self {
+        return 65536 * (rgb.r as u32) + 256 * (rgb.g as u32) + (rgb.b as u32);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::rgb::RGB;
+
+    #[test]
+    fn rgb_u32_test() {
+        let u32val: u32 = 0xFFFFFF;
+
+        let rgb = RGB::from(u32val);
+
+        assert_eq!(u32::from(rgb), u32val);
+        assert_eq!(
+            rgb,
+            RGB {
+                r: u8::MAX,
+                g: u8::MAX,
+                b: u8::MAX
+            }
+        );
+    }
+}
+
 impl fmt::Display for RGB {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}, {}, {}", self.r, self.g, self.b)
